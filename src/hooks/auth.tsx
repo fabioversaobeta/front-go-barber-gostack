5db66e6119d7ee176a15665c8b6b74804e24ef30
Fabio Bandacheski <fabio.versao.beta@gmail.com>
2020-08-13 21:11:05 -0300

    trocar de avatar

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -22,6 +22,7 @@ interface AuthContextData {
     user: User;
     signIn(credentials: SignInCredentials): Promise<void>;
     signOut(): void;
+    updateUser(user: User): void;
 }
 
 const AuthContext = createContext<AuthContextData>(
@@ -65,9 +66,21 @@ const AuthProvider: React.FC = ({ children }) => {
         setData({} as AuthState);
     }, []);
 
+    const updateUser = useCallback(
+      (user: User) => {
+        localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+        setData({
+          token: data.token,
+          user,
+        });
+      },
+      [setData, data.token]
+    );
+
     return (
       <AuthContext.Provider
-        value={{ user: data.user, signIn, signOut }}>
+        value={{ user: data.user, signIn, signOut, updateUser }}>
         {children}
       </AuthContext.Provider>
     );
